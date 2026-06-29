444e97a1002ba9a862874bf083a994937a110073
Murilo <murilobento@gmail.com>
2026-06-29 18:09:51 -0300

    chore: update dependencies and devDependencies in package.json; fix import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-/// <reference types="vitest/config" />
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
